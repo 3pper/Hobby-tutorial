3e74cc31c755fd5da2b178e9d625a301c1f53d7f
Egor Denisenko <lodpsaf@gmail.com>
2023-09-14 15:43:01 +0300

    upload

---
+++ Hobby Tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hobby Tutorial
+//
+//  Created by Egor on 14.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
